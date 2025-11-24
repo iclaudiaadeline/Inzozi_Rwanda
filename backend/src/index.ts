f44021bc9fc245bd6a6b3e36f8b2a2c15c51c758
iclaudiaadeline <a.iradukund3@alustudent.com>
2025-11-24 13:55:16 +0200

    last

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const CORS_ORIGIN = process.env.CORS_ORIGIN || 'http://localhost:5173';
+const CORS_ORIGIN = 'https://inzozi-rwanda.onrender.com/';
 
 // Middleware
 app.use(cors({
